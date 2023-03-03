e5ab819b10e87b8e77f92304863f030ca7a8b081
Arthur Lee <arthur@broadinstitute.org>
2023-03-03 00:33:24 -0500

    Update long_names.R

--- long_names.R
+++ long_names.R
@@ -1,8 +1,11 @@
 ## long_names.R
-## usage: Rscript long_names.R
+## usage: Rscript long_names.R <path to .json>
 
 library(jsonlite)
-x <- fromJSON("/Users/arthur/coding/routes.json") ## wget -O routes.json https://api-v3.mbta.com/routes
+args <- commandArgs(trailingOnly = TRUE)
+path <- args[1]
+
+x <- fromJSON(path) ## wget -O routes.json https://api-v3.mbta.com/routes
 
 ## answer 1: For this exercise I will download entire set and filter locally. This is my choice because this gives me the option to customise filtering terms if necessary in the future. This approach also gives me a consistent reference in case the data changes in the future. I may decide to also repeat filtering using the API just as a sanity check. In some cases using the API might be preferable. For example if the filesizes are prohibitively large or if egress is expensive.
 
